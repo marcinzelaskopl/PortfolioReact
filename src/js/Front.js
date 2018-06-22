5ce364061dd526ba0c0f608427a2c12c05bb31ce
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-06-22 15:02:52 +0200

    test

--- src/js/Front.js
+++ src/js/Front.js
@@ -16,7 +16,7 @@ class Main extends React.Component{
                         <h1>More coding</h1>
                         <h2>More fun!</h2>
                         <p>Hi, I'm Marcin, wannabe developer</p>
-                        <Link to='/navigation' className={'button-front'}>Check my projects! &#187;</Link>
+                        <Link to='/navigation' className={'button-front'}>Check my projects!!! &#187;</Link>
                     </div>
                 </div>
             </div>
